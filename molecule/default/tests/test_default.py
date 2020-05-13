6284463c1edddf176cf72e6f736ac1c541b3987a
Charlie Mordant <cmordant1@gmail.com>
2020-05-13 19:31:22 +0200

    lint

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -14,24 +14,28 @@ def test_vagrant_machine_is_running(host):
 
 
 def test_pool_is_started(host):
-    command = r""" sudo virsh pool-list --all | cut -d " " -f 5 | tail -n +3 | head -n +1 | egrep -c '^active$'"""
+    command = r""" sudo virsh pool-list --all |
+    cut -d " " -f 5 | tail -n +3 | head -n +1 | egrep -c '^active$'"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
 
 
 def test_pool_is_autostarted(host):
-    command = r""" sudo virsh pool-list --all | cut -d " " -f 8 | tail -n +3 | head -n +1 | egrep -c '^yes$'"""
+    command = r""" sudo virsh pool-list --all |
+    cut -d " " -f 8 | tail -n +3 | head -n +1 | egrep -c '^yes$'"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
 
 
 def test_net_is_started(host):
-    command = r""" sudo virsh net-list --all | cut -d " " -f 5 | tail -n +3 | head -n +1 | egrep -c '^active$'"""
+    command = r""" sudo virsh net-list --all |
+    cut -d " " -f 5 | tail -n +3 | head -n +1 | egrep -c '^active$'"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
 
 
 def test_net_is_autostarted(host):
-    command = r""" sudo virsh net-list --all | cut -d " " -f 8 | tail -n +3 | head -n +1 | egrep -c '^yes$'"""
+    command = r""" sudo virsh net-list --all |
+    cut -d " " -f 8 | tail -n +3 | head -n +1 | egrep -c '^yes$'"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
