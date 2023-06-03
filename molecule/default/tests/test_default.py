61f4dd8c7f33ba52d8212ea92878bd1b528cf53a
Charlie Mordant <cmordant1@gmail.com>
2023-06-04 01:23:43 +0200

    [skip travis] normalize molecule.yml files
    Signed-off-by: Charlie Mordant <cmordant1@gmail.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,14 +2,14 @@
 
 
 def test_vagrant_machine_is_running(host):
-    command = r"""vagrant status | egrep -c 'myvm\s*running\s\(libvirt\)'"""
+    command = r"""vagrant status | grep -Ec 'myvm\s*running\s\(libvirt\)'"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
 
 
 def test_pool_is_started(host):
     command = r"""virsh pool-list --all |
-    cut -d " " -f 5 | tail -n +3 | head -n +1 | egrep -c '^active$'"""
+    cut -d " " -f 5 | tail -n +3 | head -n +1 | grep -Ec '^active$'"""
     with host.sudo():
         cmd = host.run(command)
         assert '1' in cmd.stdout
@@ -17,7 +17,7 @@ def test_pool_is_started(host):
 
 def test_pool_is_autostarted(host):
     command = r"""virsh pool-list --all |
-    cut -d " " -f 8 | tail -n +3 | head -n +1 | egrep -c '^yes$'"""
+    cut -d " " -f 8 | tail -n +3 | head -n +1 | grep -Ec '^yes$'"""
     with host.sudo():
         cmd = host.run(command)
         assert '1' in cmd.stdout
